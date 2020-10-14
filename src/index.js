96d6ca15ac216c60c9c2fd516385b825d5b0bc5d
Siarhei Kazlouski <aspirin12@yandex.ru>
2020-10-14 17:21:49 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    // split expr into 10 bits pieces
+    arr = expr.match(/.{1,10}/g);
+
+    // delete zeros 
+    arr.forEach(function(item, index){
+        this[index] = item.replace(/^0+/, "");
+    }, arr);
+      
+    // replace asterisks with spaces
+    arr.forEach(function(item, index){
+        this[index] = item.replace(/\*+/, " ");
+    }, arr);
+     
+    // replace 10s with dots and 11s with dashes
+    arr.forEach(function(item, index){
+        this[index] = item.replace(/10/g, ".").replace(/11/g, "-");
+    }, arr);
+
+
+    // decode 
+    arr.forEach(function(item, index){
+        this[index] = MORSE_TABLE[arr[index]] || " "; // put space if no letters found in MORSE_TABLE;
+    }, arr);
+      
+    return arr.join('');
+
 }
 
 module.exports = {
